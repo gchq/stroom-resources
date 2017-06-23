c005de0016ffca411a44f8c664f818b4ab01dcbe
at055612 <at055612@gmail.com>
2017-06-23 19:25:47 +0100

    Improve bouceIt script to allow additional params, improve usage txt

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
+if [ "$#" -eq 0 ] || ! [ -f "$1" ]; then
   echo "Usage: $0 dockerComposeYmlFile" >&2
+  echo "E.g: $0 compose/everything.yml" >&2
+  echo "Possible compose files:" >&2
+  ls -1 ./compose/*.yml
   exit 1
 fi
 
@@ -10,4 +13,5 @@ projectName=`basename $ymlFile | sed 's/\.yml$//'`
 
 echo "Bouncing project $projectName with using $ymlFile"
 
-docker-compose -f $ymlFile down && docker-compose -f $ymlFile -p $projectName up --build
+#pass any additional arguments after the yml filename direct to docker-compose
+docker-compose -f $ymlFile down && docker-compose -f $ymlFile -p $projectName up --build ${*:2}
