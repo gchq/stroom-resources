93da2c97219bcaa1670b0afde7745a6b2ef3dcb5
at055612 <at055612@gmail.com>
2017-08-30 11:57:17 +0100

    Add /etc/hosts inspection to bounceIt script

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LOCAL_HOST_NAMES="kafka hbase"
 
 if [ "$#" -eq 0 ] || ! [ -f "$1" ]; then
   echo "Usage: $0 dockerComposeYmlFile" >&2
@@ -10,6 +11,25 @@ fi
 
 ymlFile=$1
 projectName=`basename $ymlFile | sed 's/\.yml$//'`
+
+isHostMissing=""
+
+#Some of the docker containers required entires in your local hosts file to
+#work correctly. This code checks they are all there
+for host in $LOCAL_HOST_NAMES; do
+    if [ $(cat /etc/hosts | grep "127.0.0.1 $host" | wc -l) -eq 0 ]; then 
+        echo "ERROR - /etc/hosts is missing an entry for \"127.0.0.1 $host\""
+        isHostMissing=true
+        echo "Add the following line to /etc/hosts:"
+        echo "127.0.0.1 $host"
+        echo
+    fi
+done
+
+if [ $isHostMissing ]; then
+    echo "Quting!"
+    exit 1
+fi
 
 #Ensure we have the latest image of stroom from dockerhub
 #Needed for floating tags like *-SNAPSHOT or v6
