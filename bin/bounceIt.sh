1b468d484bbe79f452e7611bd2a90195cfee713a
jc064522 <jc064522@gmail.com>
2017-09-20 19:03:28 +0100

    Fix for multi-machine deployment

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -13,15 +13,21 @@ projectName=`basename $ymlFile | sed 's/\.yml$//'`
 
 echo "Bouncing project $projectName with using $ymlFile"
 
+# We need to know where we're running this from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Creates config files from template - adding in the correct IP address
+# We need the IP to transpose into our config
 ip=`ip route get 1 | awk '{print $NF;exit}'`
+
+# This is used by the docker-compose YML files, so they can tell a browser where to go
+echo "Using the following IP as the advertised host: $ip"
+export STROOM_RESOURCES_ADVERTISED_HOST=$ip
+
+# NGINX: creates config files from templates, adding in the correct IP address
 deployRoot=$DIR/"../deploy"
 echo "Creating nginx/nginx.conf using $ip"
 sed -e 's/<SWARM_IP>/'$ip'/g' $deployRoot/template/nginx.conf > $deployRoot/nginx/nginx.conf
 sed -i 's/<STROOM_URL>/'$ip'/g' $deployRoot/nginx/nginx.conf
 sed -i 's/<AUTH_UI_URL>/'$ip'/g' $deployRoot/nginx/nginx.conf
 
-#pass any additional arguments after the yml filename direct to docker-compose
+# Pass any additional arguments after the yml filename direct to docker-compose
 docker-compose -f $ymlFile down && docker-compose -f $ymlFile -p $projectName up --build ${*:2}
