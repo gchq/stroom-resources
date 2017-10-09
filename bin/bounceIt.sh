5bbe3521d631d7f18d43b81fbaa624d4c23910a7
GCHQ-11 <gchq.developer.11@gmail.com>
2017-10-09 09:30:34 +0100

    adding annotations and auth to the tag handling section

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -139,6 +139,14 @@ if grep -q "${TAG_VARIABLE_REGEX}" $ymlFile; then
             setDockerTagValue "stroom" "STROOM_TAG" "${STROOM_TAG}"
         elif [ "$tag" = "STROOM_STATS_TAG" ]; then
             setDockerTagValue "stroom-stats" "STROOM_STATS_TAG" "${STROOM_STATS_TAG}"
+        elif [ "$tag" = "STROOM_ANNOTATIONS_SERVICE_TAG" ]; then
+            setDockerTagValue "stroom-annotations-service" "STROOM_ANNOTATIONS_SERVICE_TAG" "${STROOM_ANNOTATIONS_SERVICE_TAG}"
+        elif [ "$tag" = "STROOM_ANNOTATIONS_UI_TAG" ]; then
+            setDockerTagValue "stroom-annotations-ui" "STROOM_ANNOTATIONS_UI_TAG" "${STROOM_ANNOTATIONS_UI_TAG}"
+        elif [ "$tag" = "STROOM_AUTH_SERVICE_TAG" ]; then
+            setDockerTagValue "stroom-auth-service" "STROOM_AUTH_SERVICE_TAG" "${STROOM_AUTH_SERVICE_TAG}"
+        elif [ "$tag" = "STROOM_AUTH_UI_TAG" ]; then
+            setDockerTagValue "stroom-auth-ui" "STROOM_AUTH_UI_TAG" "${STROOM_AUTH_UI_TAG}"
         fi
     done
 
