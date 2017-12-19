c299ec31a54c4b120c59395c1d9627fad8bcd05a
at055612 <at055612@gmail.com>
2017-12-19 12:11:30 +0000

    Add -f arg, rename to local.env, other improvements

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # 
 # Script to aid the running of the docker containers used in the stroom family.
 # The script will validate 
@@ -15,7 +15,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOCAL_HOST_NAMES="kafka hbase"
 
 #Location of the file used to define the docker tag variable values
-TAGS_FILE="${SCRIPT_DIR}/docker.tags"
+TAGS_FILE="${SCRIPT_DIR}/local.env"
 
 #The docker-compose yml file that defines all the docker services for the whole stroom family
 ALL_SERVICES_COMPOSE_FILE="${SCRIPT_DIR}/compose/everything.yml"
@@ -39,26 +39,93 @@ DOCKER_TAGS_URL_SUFFIX="/tags/${NC}"
 SUPPORTED_COMPOSE_CMDS_REGEX="(start|stop|restart|up|down)"
 DEFAULT_COMPOSE_CMD="up"
 
-showUsage() {
-    echo "Usage: $0 [COMPOSE_COMMAND] [OPTION]... [EXTRA_COMPOSE_ARG]... [SERVICE_NAME]..."
-    echo "COMPOSE_COMMAND - One of ${SUPPORTED_COMPOSE_CMDS_REGEX}, if not supplied a \"stop\" and then \"${DEFAULT_COMPOSE_CMD}\" will be performed"
-    echo "OPTIONs:"
-    echo "  -h - Show this help text"
-    echo "  -e - Rely on existing environment variables for any docker tags, the docker.tags file will not be used"
-    echo "  -x - Do not check hosts file for docker related entries"
-    echo "  -y - Do not prompt for confirmation, e.g. when run from a script"
-    echo "EXTRA_COMPOSE_ARGs - Any additional arguments for docker-compose, of the form \"--argName\", e.g. \"--build\""
-    echo "e.g.: $0 serviceX serviceY"
-    echo "e.g.: $0 up -e -y --build --verbose serviceX serviceY"
-}
-
-
 printValidServiceNames() {
     echo "Valid service names are:"
     echo
-    for serviceName in $(docker-compose -f ${ymlFile} config --services | sort); do
+    for serviceName in $allServices; do
         echo -e "  ${GREEN}${serviceName}${NC}"
     done
+}
+
+showUsage() {
+    echo -e "Usage: ${BLUE}$0 [COMPOSE_COMMAND] [OPTION]... [EXTRA_COMPOSE_ARG]... [SERVICE_NAME]...${NC}"
+    echo -e "COMPOSE_COMMAND - One of ${SUPPORTED_COMPOSE_CMDS_REGEX}, if not supplied a \"stop\" and then \"${DEFAULT_COMPOSE_CMD}\" will be performed"
+    echo -e "OPTIONs:"
+    echo -e "  ${GREEN}-e${NC} - Rely on existing environment variables for any docker tags, the docker.tags file will not be used"
+    echo -e "  ${GREEN}-f${NC} - Use custom file to supply service names, tags and environment values, e.g. \"${BLUE}-f ./stroom5.env${NC}\""
+    echo -e "  ${GREEN}-h${NC} - Show this help text"
+    echo -e "  ${GREEN}-i${NC} - Do not check dockerhub for my recent versions of tagged images"
+    echo -e "  ${GREEN}-x${NC} - Do not check hosts file for docker related entries"
+    echo -e "  ${GREEN}-y${NC} - Do not prompt for confirmation, e.g. when run from a script"
+    echo -e "  NOTE: if nether -f or -e are specified the file ${BLUE}${TAGS_FILE}${NC} will be used (and created if it doesn't exist)"
+    echo -e "EXTRA_COMPOSE_ARGs - Any additional arguments for docker-compose, of the form \"${BLUE}--argName${NC}\", e.g. \"${BLUE}--build${NC}\""
+    echo -e "e.g.: ${BLUE}$0 serviceX serviceY${NC}"
+    echo -e "e.g.: ${BLUE}$0 up -e -y --build --verbose serviceX serviceY${NC}"
+    echo -e "e.g.: ${BLUE}$0 up -f stroom5.env${NC}"
+    echo
+    printValidServiceNames
+}
+
+createOrUpdateLocalTagsFile() {
+    #read all the container yml files to find any _TAG variables and convert them from something like:
+    #${STROOM_ANNOTATIONS_SERVICE_TAG:-v0.1.5-alpha.4}
+    #into something like:
+    #STROOM_ANNOTATIONS_SERVICE_TAG=v0.1.5-alpha.4
+    #If the variable has no default part (e.g. ${..._TAG}) then just use 'master-SNAPSHOT'
+    local defaultTags=$(cat ${SCRIPT_DIR}/compose/containers/*.yml | grep -o "\${.*_TAG.*}" | uniq | sed -E 's/\$\{(.*_TAG):?-?(.*)}/\1=\2/' | sed -E 's/(_TAG=)$/\1master-SNAPSHOT/')
+    #Ensure we have a TAGS_FILE file, if not create one using the content of the defaultTags string
+    if [ ! -f ${TAGS_FILE} ]; then
+        echo -e "Default docker tags file (${BLUE}${TAGS_FILE}${NC}) doesn't exist so have created it with the following content"
+        touch "${TAGS_FILE}"
+        echo "$DEFAULT_TAGS_HEADER" > $TAGS_FILE
+        echo -e "$defaultTags" >> $TAGS_FILE
+        echo
+        cat $TAGS_FILE
+        echo
+    else
+        #File exists, make sure all required tags are defined
+        #Loop round all entries in defaultTags, ignoring the top comment line
+        #assumes no spaces in 'tag_name=version'
+        for entry in $(echo -e "${defaultTags}" | egrep -v "^#.*\n") ; do
+            #echo "entry is [$entry]"
+            if [[ "${entry}" =~ _TAG= ]]; then
+                #extract the tag name from the default tags entry e.g. "    STROOM_TAG=master-SNAPSHOT   " => "STROOM_TAG"
+                tagName="$(echo "${entry}" | grep -o "[A-Z0-9_]*_TAG")"
+                #echo "tagName is $tagName"
+                #check if tagName doesn't exist in the file (in un-commented form) and if it doesn't exist, add it
+                if ! grep -q "^\s*${tagName}" "${TAGS_FILE}"; then
+                    #un-commented tagName doesn't exist in TAGS_FILE so add it
+                    echo -e "Adding ${GREEN}${entry}${NC} to file ${BLUE}${TAGS_FILE}${NC}"
+                    echo
+                    echo "${entry}" >> "${TAGS_FILE}"
+                fi
+            fi
+        done
+    fi
+}
+
+exportFileContents() {
+    local file=$1
+    if [ ! -f $file ]; then
+        echo -e "${RED}File ${file} doesn't exist${NC}" >&2
+        exit 1
+    fi
+
+    echo
+    echo -e "Using file ${BLUE}${file}${NC} to resolve any docker tags and other variables"
+
+    #export all un-commented entires in the file as environment variables so they are available to docker-compose to do variable substitution
+    local vars=$(cat ${file} | egrep "^[^#=]+=.*" | sed -E 's/([^=]+=)(.*)/export \1\2/') 
+    #local vars=$(cat ${file} | egrep "^[^#=]+=.*")
+    #echo -e "vars: $vars"
+
+    #echo "$vars" | \
+    #while read line; do 
+        #echo "${line}"
+    #done
+
+    source <(echo $vars)
+    echo 
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~start~of~script~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -69,10 +136,13 @@ isHostMissing=false
 extraComposeArguments=""
 requireConfirmation=true
 requireHostFileCheck=true
+requireLatestImageCheck=true
 useEnvironmentVariables=false
 runStopCmdFirst=false
 ymlFile=${ALL_SERVICES_COMPOSE_FILE}
 projectName=$(basename $ymlFile | sed 's/\.yml$//')
+allServices=$(docker-compose -f ${ymlFile} config --services | sort)
+customEnvFile=""
 
 if [[ "$1" =~ $SUPPORTED_COMPOSE_CMDS_REGEX ]]; then
     composeCmd=$1
@@ -86,7 +156,7 @@ fi
 
 extraComposeArguments=""
 
-optspec=":a:ehy-:"
+optspec=":ef:hiy-:"
 #The following code to parse long args, e.g. --build, is derived from an answer in
 #https://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options
 while getopts "$optspec" optchar; do
@@ -94,24 +164,33 @@ while getopts "$optspec" optchar; do
     case "${optchar}" in
         -)
             #An additional double-dash docker-compose argument
-            #echo "Using additionl compose argument --${OPTARG}"
-            extraComposeArguments="${extraComposeArguments} --${OPTARG}"
+            extraComposeArguments+=" --${OPTARG}"
             ;;
         e)
-            #echo "Using environment variables"
             useEnvironmentVariables=true
+            ;;
+        f)
+            if [ "${OPTARG}x" = "x" ]; then
+                echo -e "${RED}-f argument requires a file path to be specified${NC}" >&2
+                echo
+                showUsage
+                exit 1
+            fi
+            customEnvFile="${OPTARG}"
+            echo "Using customEnvFile: $customEnvFile"
             ;;
         h)
             #help
             showUsage
             exit 1
             ;;
+        i)
+            requireLatestImageCheck=false
+            ;;
         x)
-            #echo "Will not check hosts file"
             requireHostFileCheck=false
             ;;
         y)
-            #echo "Will not prompt for confirmation"
             requireConfirmation=false
             ;;
         *)
@@ -125,14 +204,52 @@ done
 
 #discard the args parsed so far
 shift $((OPTIND -1))
-serviceNames="$@"
+serviceNamesFromArgs="$@"
+#strip any leading whitespace
+extraComposeArguments=$(echo "$extraComposeArguments" | sed -r 's/^\s//')
 
-if [ $# = 0 ]; then
-    echo -e "${RED}No SERVICE_NAMEs specified${NC}" >&2
+if $useEnvironmentVariables && [ -n "$customEnvFile" ]; then
+    echo -e "${RED}Cannot use -f and -e arguments together${NC}" >&2
     showUsage
-    echo 
-    printValidServiceNames
     exit 1
+fi
+
+if [ ! -f $customEnvFile ]; then
+    echo -e "${RED}File ${customEnvFile} does not exist${NC}" >&2
+    exit 1
+fi
+
+if [ -n "$customEnvFile" ]; then
+    #custom env file
+    exportFileContents "${customEnvFile}"
+    if [ -z "${SERVICE_LIST}" ]; then
+        echo -e "${RED}Warning${NC}: SERVICE_LIST has not been defined in file ${customEnvFile}"
+    fi
+elif ! $useEnvironmentVariables; then
+    #default local env file
+    createOrUpdateLocalTagsFile
+    exportFileContents "${TAGS_FILE}"
+else
+    echo
+    echo "Using environment variables to resolve any docker tags and other variables"
+fi
+
+#Try setting the service names list from the SERVICE_LIST env var, which may/may not be set.
+serviceNames="${SERVICE_LIST}"
+if [ -n "${serviceNamesFromArgs}" ] && [ -n "${SERVICE_LIST}" ]; then
+    echo -e "Overriding service names from ${BLUE}${SERVICE_LIST}${NC} [${GREEN}${SERVICE_LIST}${NC}] with those from the command line [${GREEN}${serviceNamesFromArgs}${NC}]"
+    serviceNames="${serviceNamesFromArgs}"
+fi
+
+if [ "${serviceNames}x" = "x" ]; then
+    echo
+    echo -e "No service names specified, the COMPOSE_COMMAND will be applied to all services" >&2
+
+    #build a space delim list of services so we can later display all the images in use
+    for service in $allServices; do
+        serviceNames+=" $service"
+    done
+    serviceNames=$(echo "$serviceNames" | sed -r 's/^\s//')
 else
     validServiceNameRegex=""
     for serviceName in $(docker-compose -f ${ymlFile} config --services | sort); do
@@ -140,7 +257,7 @@ else
     done
 
     #strip leading pipe char
-    validServiceNameRegex=$(echo "$validServiceNameRegex" | sed -r 's/\|//')
+    validServiceNameRegex=$(echo "$validServiceNameRegex" | sed -r 's/^\|//')
     validServiceNameRegex="(${validServiceNameRegex})"
 
     for serviceName in $serviceNames; do
@@ -154,15 +271,10 @@ else
             echo -e "${RED}SERVICE_NAME [${GREEN}${serviceName}${RED}] is not valid${NC}" >&2
             echo
             showUsage
-            echo 
-            printValidServiceNames
             exit 1
         fi
     done
 fi
-
-#strip any leading whitespace
-extraComposeArguments=$(echo "$extraComposeArguments" | sed -r 's/\s//')
 
 if $requireHostFileCheck; then
     #Some of the docker containers required entries in your local hosts file to
@@ -182,53 +294,11 @@ if $requireHostFileCheck; then
     fi
 fi
 
-if ! $useEnvironmentVariables; then
-    #read all the container yml files to find any _TAG variables and convert them from something like:
-    #${STROOM_ANNOTATIONS_SERVICE_TAG:-v0.1.5-alpha.4}
-    #into something like:
-    #STROOM_ANNOTATIONS_SERVICE_TAG=v0.1.5-alpha.4
-    #If the variable has no dfeault part (e.g. ${..._TAG}) then just use 'master-SNAPSHOT'
-    defaultTags=$(cat ${SCRIPT_DIR}/compose/containers/*.yml | grep -o "\${.*_TAG.*}" | uniq | sed -E 's/\$\{(.*_TAG):?-?(.*)}/\1=\2/' | sed -E 's/(_TAG=)$/\1master-SNAPSHOT/')
-    #Ensure we have a docker.tags file, if not create one using the content of the defaultTags string
-    if [ ! -f ${TAGS_FILE} ]; then
-        echo -e "Default docker tags file (${BLUE}${TAGS_FILE}${NC}) doesn't exist so have created it with the following content"
-        touch "${TAGS_FILE}"
-        echo "$DEFAULT_TAGS_HEADER" > $TAGS_FILE
-        echo -e "$defaultTags" >> $TAGS_FILE
-        echo
-        cat $TAGS_FILE
-        echo
-    else
-        #File exists, make sure all required tags are defined
-        #Loop round all entries in defaultTags, ignoring the top comment line
-        #assumes no spaces in 'tag_name=version'
-        for entry in $(echo -e "${defaultTags}" | egrep -v "^#.*\n") ; do
-            #echo "entry is [$entry]"
-            if [[ "${entry}" =~ "_TAG=" ]]; then
-                #extract the tag name from the default tags entry e.g. "    STROOM_TAG=master-SNAPSHOT   " => "STROOM_TAG"
-                tagName="$(echo "${entry}" | grep -o "[A-Z0-9_]*_TAG")"
-                #echo "tagName is $tagName"
-                #check if tagName doesn't exist in the file (in un-commented form) and if it doesn't exist, add it
-                if ! grep -q "^\s*${tagName}" "${TAGS_FILE}"; then
-                    #un-commented tagName doesn't exist in TAGS_FILE so add it
-                    echo -e "Adding ${GREEN}${entry}${NC} to file ${BLUE}${TAGS_FILE}${NC}"
-                    echo
-                    echo "${entry}" >> "${TAGS_FILE}"
-                fi
-            fi
-        done
-    fi
-
-    #export all entires in the tags file as environment variables so they are available to docker-compose to do variable substitution
-    for entry in $(cat "${TAGS_FILE}" | egrep -v "^#.*") ; do
-        #echo "Exporting ${entry}"
-        export "${entry}"
-    done
-fi
 
 #'docker-compose config' will perform any tag substitution so the tags here will have come from the TAGS_FILE or env vars or defaults
 allImages=$(docker-compose -f $ymlFile config | egrep "image: ")
 
+echo
 echo "The following Docker services and tags will be used:"
 echo
 
@@ -255,47 +325,42 @@ for serviceName in ${serviceNames}; do
     fi
 done
 
-for serviceName in ${serviceNames}; do
-    image=$(echo "$allImages" | grep "${serviceName}:" | sed 's/.*image: //')
+if $requireLatestImageCheck; then
+    for serviceName in ${serviceNames}; do
+        image=$(echo "$allImages" | grep "${serviceName}:" | sed 's/.*image: //')
 
-    #Ensure we have the latest image of stroom from dockerhub, unless our TAG contains LOCAL
-    #Needed for floating tags like *-SNAPSHOT or v6
+        #Ensure we have the latest image of stroom from dockerhub, unless our TAG contains LOCAL
+        #Needed for floating tags like *-SNAPSHOT or v6
 
-    #method to pull updated image files from dockerhub if required
-    #This is to support -SNAPSHOT tags that are floating
-    if [ "${image}x" != "x" ]; then
-        if grep -q "${tagName}=.*LOCAL.*" "$TAGS_FILE" ; then
-            echo
-            echo -e "${GREEN}${image}${NC} is a locally built image, DockerHub will not be checked for a new version"
-        else
-            #use 'docker-compose ps' to establish if we already have a container for this service
-            #if we do then we won't do a docker-compose pull as that would trash any local state
-            #if a user wants refreshed images from dockerhub then they should delete their containers first
-            #using the dockerTidyUp script or similar
-            existingContainerId=$(docker-compose -f "$ymlFile" -p "$projectName" ps -q ${serviceName})
-            if [ "x" = "${existingContainerId}x" ]; then
-                #no existing container so do a pull to check for updates
-                #If the image has a fixed tag version e.g. master-20171008-DAILY, then no change will be
-                #detected
+        #method to pull updated image files from dockerhub if required
+        #This is to support -SNAPSHOT tags that are floating
+        if [ "${image}x" != "x" ]; then
+            if [[ "${image}" =~ .*LOCAL.* ]]; then
+            #if grep -q "${tagName}=.*LOCAL.*" "$TAGS_FILE" ; then
                 echo
-                echo -e "Checking for any updates to the ${GREEN}${serviceName}${NC} image on dockerhub"
-                #docker-compose -f "$ymlFile" -p "$projectName" pull ${repoName}
+                echo -e "${GREEN}${image}${NC} is a locally built image, DockerHub will not be checked for a new version"
             else
-                echo
-                echo -e "${GREEN}${serviceName}${NC} already has a container with ID ${BLUE}${existingContainerId}${NC}, won't check dockerhub for updates"
+                #use 'docker-compose ps' to establish if we already have a container for this service
+                #if we do then we won't do a docker-compose pull as that would trash any local state
+                #if a user wants refreshed images from dockerhub then they should delete their containers first
+                #using the dockerTidyUp script or similar
+                existingContainerId=$(docker-compose -f "$ymlFile" -p "$projectName" ps -q ${serviceName})
+                if [ "x" = "${existingContainerId}x" ]; then
+                    #no existing container so do a pull to check for updates
+                    #If the image has a fixed tag version e.g. master-20171008-DAILY, then no change will be
+                    #detected
+                    echo
+                    echo -e "Checking for any updates to the ${GREEN}${serviceName}${NC} image on dockerhub"
+                    #docker-compose -f "$ymlFile" -p "$projectName" pull ${repoName}
+                else
+                    echo
+                    echo -e "${GREEN}${serviceName}${NC} already has a container with ID ${BLUE}${existingContainerId}${NC}, won't check dockerhub for updates"
+                fi
             fi
         fi
-    fi
-done
-
-if $useEnvironmentVariables ; then
-    echo
-    echo "Using environment variables to resolve any docker tag variables"
-else
-    echo
-    echo -e "Docker tags can be changed in the file ${BLUE}${TAGS_FILE}${NC} in the form:"
-    echo -e "  ${YELLOW}xxxxxxx_TAG=master-SNAPSHOT${NC}"
+    done
 fi
+
 
 # We need the IP to transpose into our config
 
@@ -343,7 +408,7 @@ fi
 
 echo 
 if $runStopCmdFirst; then
-    echo "Ensuring ALL servcies are stopped"
+    echo "Ensuring ALL services are stopped"
     docker-compose -f $ymlFile -p $projectName stop 
 fi
 
