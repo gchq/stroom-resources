1d70e90e1a65c02c8e594eca94fad800f7fa6845
at055612 <at055612@gmail.com>
2018-01-05 11:38:13 +0000

    Improve awk script that determines the IP address

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -384,9 +384,9 @@ if [ "x${STROOM_RESOURCES_ADVERTISED_HOST}" != "x" ]; then
 else
     if [ "$(uname)" == "Darwin" ]; then
         # Code required to find IP address is different in MacOS
-        ip=`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'`
+        ip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
     else
-        ip=`ip route get 1 | awk '{print $NF;exit}'`
+        ip=$(ip route get 1 |awk 'match($0,"src [0-9\\.]+") {print substr($0,RSTART+4,RLENGTH-4)}')
     fi
     echo
     echo -e "Using IP ${GREEN}${ip}${NC} as the advertised host, as determined from the operating system"
