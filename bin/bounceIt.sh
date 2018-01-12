1a497ee5e88c2fde8dc933fb1bab596458d27d94
GCHQ-11 <gchq.developer.11@gmail.com>
2018-01-12 11:33:57 +0000

    query elastic and auth now using the config.json method for config, also giving ourselves dev URLs

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -17,7 +17,7 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #List of hostnames that need to be added to /etc/hosts to resolve to 127.0.0.1
-LOCAL_HOST_NAMES="kafka hbase stroom-auth-service stroom-auth-ui"
+LOCAL_HOST_NAMES="kafka hbase stroom-auth-service stroom-auth-ui stroom-annotations-service stroom-annotations-ui stroom-query-elastic-service stroom-query-elastic-ui"
 
 #Location of the file used to define the docker tag variable values
 TAGS_FILE="${SCRIPT_DIR}/local.env"
