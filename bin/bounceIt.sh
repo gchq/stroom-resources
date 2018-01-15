5e505197fbd43fb3d76df93e4735d848dcbff8cd
at055612 <at055612@gmail.com>
2018-01-15 12:35:39 +0000

    Move the code that establishes the advertised host before env file eval

--- bin/bounceIt.sh
+++ bin/bounceIt.sh
@@ -240,6 +240,33 @@ if [ ! -f $customEnvFile ]; then
     exit 1
 fi
 
+# We need the IP to transpose into our config
+if [ "x${STROOM_RESOURCES_ADVERTISED_HOST}" != "x" ]; then
+    ip="${STROOM_RESOURCES_ADVERTISED_HOST}"
+    echo
+    echo -e "Using IP ${GREEN}${ip}${NC} as the advertised host, as obtained from ${BLUE}STROOM_RESOURCES_ADVERTISED_HOST${NC}"
+else
+    if [ "$(uname)" == "Darwin" ]; then
+        # Code required to find IP address is different in MacOS
+        ip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
+    else
+        ip=$(ip route get 1 |awk 'match($0,"src [0-9\\.]+") {print substr($0,RSTART+4,RLENGTH-4)}')
+    fi
+    echo
+    echo -e "Using IP ${GREEN}${ip}${NC} as the advertised host, as determined from the operating system"
+fi
+
+if [[ ! "${ip}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    echo
+    echo -e "${RED}ERROR${NC} IP address [${GREEN}${ip}${NC}] is not valid, try setting '${BLUE}STROOM_RESOURCES_ADVERTISED_HOST=x.x.x.x${NC}' in ${BLUE}local.env${NC}" >&2
+    exit 1
+fi
+
+# This is used by the docker-compose YML files, so they can tell a browser where to go
+export STROOM_RESOURCES_ADVERTISED_HOST="${ip}"
+
+echo
+
 if [ -n "$customEnvFile" ]; then
     #custom env file
     exportFileContents "${customEnvFile}"
@@ -393,32 +420,6 @@ if $requireLatestImageCheck && [[ "${composeCmd}" =~ ${CMDS_FOR_IMAGE_CHECK} ]] 
 fi
 
 
-# We need the IP to transpose into our config
-if [ "x${STROOM_RESOURCES_ADVERTISED_HOST}" != "x" ]; then
-    ip="${STROOM_RESOURCES_ADVERTISED_HOST}"
-    echo
-    echo -e "Using IP ${GREEN}${ip}${NC} as the advertised host, as obtained from ${BLUE}STROOM_RESOURCES_ADVERTISED_HOST${NC}"
-else
-    if [ "$(uname)" == "Darwin" ]; then
-        # Code required to find IP address is different in MacOS
-        ip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
-    else
-        ip=$(ip route get 1 |awk 'match($0,"src [0-9\\.]+") {print substr($0,RSTART+4,RLENGTH-4)}')
-    fi
-    echo
-    echo -e "Using IP ${GREEN}${ip}${NC} as the advertised host, as determined from the operating system"
-fi
-
-if [[ ! "${ip}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    echo
-    echo -e "${RED}ERROR${NC} IP address [${GREEN}${ip}${NC}] is not valid, try setting '${BLUE}STROOM_RESOURCES_ADVERTISED_HOST=x.x.x.x${NC}' in ${BLUE}local.env${NC}" >&2
-    exit 1
-fi
-
-# This is used by the docker-compose YML files, so they can tell a browser where to go
-export STROOM_RESOURCES_ADVERTISED_HOST="${ip}"
-
-echo
 
 #echo "Using the following docker images:"
 #echo
