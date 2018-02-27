b2cd5823453673169065b748910d4b975a644a97
at055612 <at055612@gmail.com>
2018-02-27 09:17:34 +0000

    Add -l arg to buildAndReleaseNginx.sh to not push to DockerHub

--- nginx/buildAndReleaseNginx.sh
+++ nginx/buildAndReleaseNginx.sh
@@ -16,20 +16,73 @@
 # limitations under the License.
 #**********************************************************************
 
+#**********************************************************************
+# Script to build the stroom-nginx docker image and release it to 
+# DockerHub
+#**********************************************************************
+
+#Shell Colour constants for use in 'echo -e'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+LGREY='\e[37m'
+DGREY='\e[90m'
+NC='\033[0m' # No Color
+
 #Stop script on first error
 set -e
 
 DOCKER_REPO=gchq/stroom-nginx
 
+showUsage() {
+    echo -e "Usage: ${BLUE}$0 [-l] dockerTag"
+    echo -e "OPTIONs:"
+    echo -e "  ${GREEN}-l${NC} - Local build only, won't push to DockerHub"
+    echo -e "e.g.: ${BLUE}$0 -l v1.2.3${NC}"
+    echo -e "e.g.: ${BLUE}$0 v1.2.3${NC}"
+    echo
+}
+
+localBuild=false
+
+optspec=":l"
+while getopts "$optspec" optchar; do
+    #echo "Parsing $optchar"
+    case "${optchar}" in
+        l)
+            localBuild=true
+            ;;
+        *)
+            echo -e "${RED}ERROR${NC} Unknown argument: '-${OPTARG}'" >&2
+            echo
+            showUsage
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
 if [ $# -ne 1 ]; then
-    echo "Must supply the version as the first argument, e.g. $0 v1.0.0_nginx-v1.13.8"
+    echo "Must supply the dockerTag"
+    showUsage
     exit 1
 fi
 
 # This script is all you need to build an image of stroom-stats.
 ver="$1"
 
+echo
+echo -e "${GREEN}Building ${BLUE}${DOCKER_REPO}:${ver}${NC}"
+echo
+
 docker build --tag ${DOCKER_REPO}:${ver} .
 
-# This assumes you have authenticated with docker using 'docker login', else it will fail
-docker push ${DOCKER_REPO}:${ver}
+if ! ${localBuild}; then
+    echo
+    echo -e "${GREEN}Pushing to DockerHub (will fail if you haven't run '${BLUE}docker login${GREEN}'${NC}"
+    echo
+    # This assumes you have authenticated with docker using 'docker login', else it will fail
+    docker push ${DOCKER_REPO}:${ver}
+fi
