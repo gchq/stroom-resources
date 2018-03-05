dc44d0f51c8a656972431ab86709c60ca780d5da
GCHQ-11 <gchq.developer.11@gmail.com>
2018-03-05 09:08:57 +0000

    adding prompt to clean up local dev database

--- bin/dockerTidyUp.sh
+++ bin/dockerTidyUp.sh
@@ -9,3 +9,12 @@ docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker 
 # remove unused volumes:
 docker volume ls -qf dangling=true | xargs -r docker volume rm
 
+# Remove the externalised database volumes
+while true; do
+    read -p "Do you wish to clean up the local dev database volumes (will prompt for root pw)? " yn
+    case $yn in
+        [Yy]* ) sudo rm -rf ~/.stroom/db_data/; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
