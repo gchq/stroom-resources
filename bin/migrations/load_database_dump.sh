a8e5479f9239d8f0844e0bd78dbabec4577d7272
jc064522 <jc064522@gmail.com>
2018-03-08 10:14:34 +0000

    Remove unnecessary parameter

--- bin/migrations/load_database_dump.sh
+++ bin/migrations/load_database_dump.sh
@@ -11,7 +11,6 @@ source $SCRIPT_DIR/../utilities.sh
 determineHostAddress
 
 readonly SQL_DUMP_FILE=$1
-readonly STROOM_FAT_JAR=$2
 readonly DATABASE_NAME=$3
 
 # Location of migration
@@ -21,7 +20,7 @@ readonly TEMP_DB_ROOT_PW=my-secret-pw
 echo "Deleting any existing migration MySQL DB"
 docker rm -f stroom-migration-test-db
 
-echo "Starting a Docker MySQL instance to test migration of ${DATABASE_NAME} with  ${SQL_DUMP_FILE} using Stroom Fat Jar ${STROOM_FAT_JAR}"
+echo "Starting a Docker MySQL instance to test migration of ${DATABASE_NAME} with ${SQL_DUMP_FILE}."
 
 echo "Starting a new MySQL container for testing migration"
 docker run -p 5506:3306 --name stroom-migration-test-db --health-cmd='mysqladmin ping --silent' --health-interval=10s -e MYSQL_ROOT_PASSWORD=${TEMP_DB_ROOT_PW} -d mysql:5.6
