c796242e19743f8cc9dfa7a539f02384a06262bf
GCHQ-11 <gchq.developer.11@gmail.com>
2018-03-13 16:27:42 +0000

    erm...really fixing dockerTidyUp for MacOSX

--- bin/dockerTidyUp.sh
+++ bin/dockerTidyUp.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # remove exited containers:
-docker ps --filter status=dead --filter status=exited -aq | xargs -I docker rm -v
+docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
     
 # remove unused images:
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -I docker rmi
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs docker rmi
 
 # remove unused volumes:
-docker volume ls -qf dangling=true | xargs -I docker volume rm
+docker volume ls -qf dangling=true | xargs docker volume rm
 
 # Remove the externalised database volumes
 while true; do
