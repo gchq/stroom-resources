75e33989f92ba1cebce9bb2c330740df7e6b6789
GCHQ-11 <gchq.developer.11@gmail.com>
2018-03-14 09:57:54 +0000

    reverting previous two fixes

--- bin/dockerTidyUp.sh
+++ bin/dockerTidyUp.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # remove exited containers:
-docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
+docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
     
 # remove unused images:
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs docker rmi
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
 
 # remove unused volumes:
-docker volume ls -qf dangling=true | xargs docker volume rm
+docker volume ls -qf dangling=true | xargs -r docker volume rm
 
 # Remove the externalised database volumes
 while true; do
