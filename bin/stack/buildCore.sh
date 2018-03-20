875f3008b90c67c448c6c1ab092c29aaca8a6e4c
jc064522 <jc064522@gmail.com>
2018-03-20 13:31:25 +0000

    Add nginx

--- bin/stack/buildCore.sh
+++ bin/stack/buildCore.sh
@@ -2,6 +2,6 @@
 #
 # Builds a core stack using the default version from the yaml.
 
-./create_stack_yaml.sh core stroom stroomDb stroomAuthService stroomAuthUi stroomAuthDb stroomStatsDb zookeeper ctop
+./create_stack_yaml.sh core stroom stroomDb stroomAuthService stroomAuthUi stroomAuthDb stroomStatsDb zookeeper ctop nginx
 ./create_stack_env.sh core
 ./create_stack_scripts.sh core
