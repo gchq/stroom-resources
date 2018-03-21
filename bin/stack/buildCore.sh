fc1dead07b025c7114fce97e8a58c3e198edfdae
jc064522 <jc064522@gmail.com>
2018-03-21 06:55:33 +0000

    Add const to reduce hard-coding

--- bin/stack/buildCore.sh
+++ bin/stack/buildCore.sh
@@ -2,6 +2,8 @@
 #
 # Builds a core stack using the default version from the yaml.
 
-./create_stack_yaml.sh core stroom stroomDb stroomAuthService stroomAuthUi stroomAuthDb stroomStatsDb zookeeper ctop nginx
-./create_stack_env.sh core
-./create_stack_scripts.sh core
+readonly STACK_NAME="core"
+
+./create_stack_yaml.sh $STACK_NAME stroom stroomDb stroomAuthService stroomAuthUi stroomAuthDb stroomStatsDb zookeeper ctop nginx
+./create_stack_env.sh $STACK_NAME
+./create_stack_scripts.sh $STACK_NAME
