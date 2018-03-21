d8a729dddc4f8c7215d8a974416c980969a3a6b8
jc064522 <jc064522@gmail.com>
2018-03-21 16:09:22 +0000

    Improve readability

--- bin/stack/buildCore.sh
+++ bin/stack/buildCore.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #
-# Builds a core stack using the default version from the yaml.
+# Builds a core Stroom stack using the default configuration from the yaml.
 
-readonly STACK_NAME="core"
+readonly STACK_NAME="stroom_core"
+readonly SERVICES="stroom stroomDb stroomAuthService stroomAuthUi stroomAuthDb stroomStatsDb zookeeper nginx"
 
-./create_stack_yaml.sh $STACK_NAME stroom stroomDb stroomAuthService stroomAuthUi stroomAuthDb stroomStatsDb zookeeper nginx
+./create_stack_yaml.sh $STACK_NAME $SERVICES
 ./create_stack_env.sh $STACK_NAME
 ./create_stack_scripts.sh $STACK_NAME
 ./create_stack_assets.sh $STACK_NAME
