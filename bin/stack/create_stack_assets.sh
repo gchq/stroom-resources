5d66c0d9fe2ff65d81c2c32f93f23c20d9d36c4d
jc064522 <jc064522@gmail.com>
2018-03-21 11:40:52 +0000

    Add a script to copy assets over

---
+++ bin/stack/create_stack_assets.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Copies the necessary assets into the build
+
+source lib/shell.sh
+
+main() {
+    readonly local STACK_NAME=$1
+    readonly local BUILD_FOLDER='build'
+    readonly local WORKING_DIRECTORY="$BUILD_FOLDER/$STACK_NAME"
+
+    readonly local SRC_CERTS_DIRECTORY="../../dev-resources/certs/server"
+    readonly local SRC_NGINX_CONF_DIRECTORY="../../stroom-nginx/template"
+    readonly local SRC_AUTH_UI_CONF_DIRECTORY="../../stroom-microservice-ui/template"
+
+    setup_echo_colours
+
+    echo -e "Copying certs for auth-ui"
+    readonly local DEST_AUTH_UI_CERTS_DIRECTORY="$WORKING_DIRECTORY/volumes/auth-ui/certs"
+    mkdir -p "$DEST_AUTH_UI_CERTS_DIRECTORY"
+    cp $SRC_CERTS_DIRECTORY/* $DEST_AUTH_UI_CERTS_DIRECTORY
+
+    echo -e "Copying conf for auth-ui"
+    readonly local DEST_AUTH_UI_CONF_DIRECTORY="$WORKING_DIRECTORY/volumes/auth-ui/conf"
+    mkdir -p "$DEST_AUTH_UI_CONF_DIRECTORY"
+    cp $SRC_AUTH_UI_CONF_DIRECTORY/* $DEST_AUTH_UI_CONF_DIRECTORY
+
+    echo -e "Copying certs for nginx"
+    readonly local DEST_NGINX_CERTS_DIRECTORY="$WORKING_DIRECTORY/volumes/nginx/certs"
+    mkdir -p "$DEST_NGINX_CERTS_DIRECTORY"
+    cp $SRC_CERTS_DIRECTORY/* $DEST_NGINX_CERTS_DIRECTORY
+
+    echo -e "Copying conf for nginx"
+    readonly local DEST_NGINX_CONF_DIRECTORY="$WORKING_DIRECTORY/volumes/nginx/conf"
+    mkdir -p "$DEST_NGINX_CONF_DIRECTORY"
+    cp $SRC_NGINX_CONF_DIRECTORY/* $DEST_NGINX_CONF_DIRECTORY
+
+}
+
+main "$@"
