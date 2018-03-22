8e23b8e0b6b3d1a05d8cb0ed80fb1705a5dca659
jc064522 <jc064522@gmail.com>
2018-03-22 14:48:43 +0000

    For the DRY

--- bin/stack/buildCore.sh
+++ bin/stack/buildCore.sh
@@ -2,10 +2,9 @@
 #
 # Builds a core Stroom stack using the default configuration from the yaml.
 
+set -e
+
 readonly STACK_NAME="stroom_core"
 readonly SERVICES="stroom stroomDb stroomAuthService stroomAuthUi stroomAuthDb stroomStatsDb zookeeper nginx"
 
-./create_stack_yaml.sh $STACK_NAME $SERVICES
-./create_stack_env.sh $STACK_NAME
-./create_stack_scripts.sh $STACK_NAME
-./create_stack_assets.sh $STACK_NAME
+./build.sh $STACK_NAME $SERVICES
