82aaebe7b93f62be666f8a27311f0f948bb60d11
at055612 <at055612@gmail.com>
2018-11-06 08:49:05 +0000

    Change start script to check for installed docker/docker-compose

--- bin/stack/lib/template_start.sh
+++ bin/stack/lib/template_start.sh
@@ -20,26 +20,47 @@ setup_echo_colours
 # available to docker-compose
 source "$DIR"/config/<STACK_NAME>.env
 
-# These lines may help in debugging the config that is passed to the containers
-#env
-#docker-compose -f "$DIR"/config/stroom_core.yml config
+check_installed_binaries() {
+    if ! command -v docker 1>/dev/null; then 
+        echo -e "${RED}ERROR${NC}: Docker CE is not installed!"
+        echo -e "See ${BLUE}https://docs.docker.com/install/#supported-platforms${NC} for details on how to install it"
+        echo -e "Version ${BLUE}17.12.0${NC} or higher is required"
+        exit 1
+    fi
 
-echo -e "${GREEN}Creating and starting the docker containers and volumes${NC}"
-#echo -e "${GREEN}Using IP address ${BLUE}${HOST_IP}${NC}"
-echo
+    if ! command -v docker-compose 1>/dev/null; then 
+        echo -e "${RED}ERROR${NC}: Docker Compose is not installed!"
+        echo -e "See ${BLUE}https://docs.docker.com/compose/install/${NC} for details on how to install it"
+        echo -e "Version ${BLUE}1.21.0${NC} or higher is required"
+        exit 1
+    fi
+}
 
-docker-compose --project-name <STACK_NAME> -f "$DIR"/config/<STACK_NAME>.yml up -d
+main() {
+    check_installed_binaries
 
-echo
-echo -e "${GREEN}Waiting for stroom to complete its start up.${NC}"
-echo -e "${DGREY}Stroom has to build its database tables when started for the first time,${NC}"
-echo -e "${DGREY}so this may take a minute or so. Subsequent starts will be quicker.${NC}"
+    # These lines may help in debugging the config that is passed to the containers
+    #env
+    #docker-compose -f "$DIR"/config/stroom_core.yml config
 
-wait_for_200_response "http://localhost:${STROOM_ADMIN_PORT}/stroomAdmin"
+    echo -e "${GREEN}Creating and starting the docker containers and volumes${NC}"
+    #echo -e "${GREEN}Using IP address ${BLUE}${HOST_IP}${NC}"
+    echo
 
-# Stroom is now up or we have given up waiting so check the health
-./health.sh
+    docker-compose --project-name <STACK_NAME> -f "$DIR"/config/<STACK_NAME>.yml up -d
 
-# Display the banner, URLs and login details
-./info.sh
+    echo
+    echo -e "${GREEN}Waiting for stroom to complete its start up.${NC}"
+    echo -e "${DGREY}Stroom has to build its database tables when started for the first time,${NC}"
+    echo -e "${DGREY}so this may take a minute or so. Subsequent starts will be quicker.${NC}"
 
+    wait_for_200_response "http://localhost:${STROOM_ADMIN_PORT}/stroomAdmin"
+
+    # Stroom is now up or we have given up waiting so check the health
+    ./health.sh
+
+    # Display the banner, URLs and login details
+    ./info.sh
+}
+
+main $@
