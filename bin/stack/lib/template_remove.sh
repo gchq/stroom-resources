7061cb00c2780f14c67828390af9e29215b612b8
at055612 <at055612@gmail.com>
2018-11-14 21:35:24 +0000

    Add -y arg to remove script

--- bin/stack/lib/template_remove.sh
+++ bin/stack/lib/template_remove.sh
@@ -9,21 +9,53 @@ readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR"/lib/shell_utils.sh
 setup_echo_colours
 
+showUsage() {
+    echo -e "Usage: ${BLUE}$0 [OPTION]...${NC}"
+    echo -e "  ${GREEN}-y${NC} - Do not prompt for confirmation, e.g. when run from a script"
     echo
-    echo -e "${RED}WARNING:${NC} ${GREEN}This will remove all the Docker containers and volumes in the stack${NC}"
-    echo -e "${GREEN}so all data (content, events, indexes, etc.) will be lost.${NC}"
-    echo
-    read -rsp $'Press "y" to continue, any other key to cancel.\n' -n1 keyPressed
+}
 
-    if [ "$keyPressed" = 'y' ] || [ "$keyPressed" = 'Y' ]; then
+main() {
+
+    local requireConfirmation=true
+    local optspec="y"
+
+    while getopts "$optspec" optchar; do
+        case "${optchar}" in
+            y)
+                requireConfirmation=false
+                ;;
+            *)
+                echo -e "${RED}ERROR${NC} Unknown argument: '-${OPTARG}'" >&2
+                echo
+                showUsage
+                exit 1
+                ;;
+        esac
+    done
+
+    if $requireConfirmation; then
+
         echo
-    else
+        echo -e "${RED}WARNING:${NC} ${GREEN}This will remove all the Docker containers and volumes in the stack${NC}"
+        echo -e "${GREEN}so all data (content, events, indexes, etc.) will be lost.${NC}"
         echo
-        echo "Exiting"
-        exit 0
+        read -rsp $'Press "y" to continue, any other key to cancel.\n' -n1 keyPressed
+
+        if [ "$keyPressed" = 'y' ] || [ "$keyPressed" = 'Y' ]; then
+            echo
+        else
+            echo
+            echo "Exiting"
+            exit 0
+        fi
     fi
 
-echo -e "${GREEN}Stopping and removing the Docker containers and volumes${NC}"
-echo
+    echo -e "${GREEN}Stopping and removing the Docker containers and volumes${NC}"
+    echo
 
-docker-compose --project-name <STACK_NAME> -f "$DIR"/config/<STACK_NAME>.yml down -v
+    docker-compose --project-name <STACK_NAME> -f "$DIR"/config/<STACK_NAME>.yml down -v
+
+}
+
+main $@
