467cb1f5d957424887df9b9e315def936f836afc
at055612 <at055612@gmail.com>
2018-11-28 17:18:42 +0000

    Change stack archive structure so volumes is a sibling of the config
    This means we can download a new stack release and keep the existing
    bind mount volumes

--- bin/stack/create_stack_assets.sh
+++ bin/stack/create_stack_assets.sh
@@ -2,6 +2,8 @@
 #
 # Copies the necessary assets into the build
 
+set -e
+
 source lib/shell_utils.sh
 
 download_file() {
@@ -10,9 +12,9 @@ download_file() {
     local -r filename=$3
 
     echo -e "  Downloading ${BLUE}${url_base}/${filename}${NC} to ${BLUE}${dest_dir}${NC}"
-    wget --quiet --directory-prefix=${dest_dir} "${url_base}/${filename}"
+    wget --quiet --directory-prefix="${dest_dir}" "${url_base}/${filename}"
     if [[ "${filename}" =~ .*\.sh$ ]]; then
-        chmod u+x ${dest_dir}/${filename}
+        chmod u+x "${dest_dir}/${filename}"
     fi
 }
 
@@ -21,87 +23,89 @@ main() {
 
     [ "$#" -ge 2 ] || die "${RED}Error${NC}: Invalid arguments, usage: ${BLUE}build.sh stackName serviceX serviceY etc.${NC}"
 
-    local -r services="${@:2}"
+    local -r services=( "${@:2}" )
 
     echo -e "${GREEN}Copying assets${NC}"
 
-    local -r STACK_NAME=$1
-    local -r BUILD_FOLDER='build'
-    local -r WORKING_DIRECTORY="${BUILD_FOLDER}/${STACK_NAME}"
+    local -r BUILD_STACK_NAME=$1
+    local -r VERSION=$2
+    local -r BUILD_DIRECTORY="build/${BUILD_STACK_NAME}"
+    local -r WORKING_DIRECTORY="${BUILD_DIRECTORY}/${BUILD_STACK_NAME}-${VERSION}"
+    local -r VOLUMES_DIRECTORY="${BUILD_DIRECTORY}/volumes"
 
     local -r SRC_CERTS_DIRECTORY="../../dev-resources/certs"
     local -r SRC_NGINX_CONF_DIRECTORY="../../stroom-nginx/template"
-    local -r VOLUMES_DIRECTORY="../../dev-resources/compose/volumes"
-    local -r SRC_ELASTIC_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/elasticsearch/conf"
-    local -r SRC_KIBANA_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/kibana/conf"
-    local -r SRC_STROOM_LOG_SENDER_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/stroom-log-sender/conf"
+    local -r SRC_VOLUMES_DIRECTORY="../../dev-resources/compose/volumes"
+    local -r SRC_ELASTIC_CONF_DIRECTORY="${SRC_VOLUMES_DIRECTORY}/elasticsearch/conf"
+    local -r SRC_KIBANA_CONF_DIRECTORY="${SRC_VOLUMES_DIRECTORY}/kibana/conf"
+    local -r SRC_STROOM_LOG_SENDER_CONF_DIRECTORY="${SRC_VOLUMES_DIRECTORY}/stroom-log-sender/conf"
     local -r SRC_AUTH_UI_CONF_DIRECTORY="../../stroom-microservice-ui/template"
     local -r SEND_TO_STROOM_VERSION="send-to-stroom-v1.3"
     local -r SEND_TO_STROOM_URL_BASE="https://raw.githubusercontent.com/gchq/stroom-clients/${SEND_TO_STROOM_VERSION}/bash"
 
-    local -r DEST_PROXY_CERTS_DIRECTORY="$WORKING_DIRECTORY/volumes/stroom-proxy/certs"
+    local -r DEST_PROXY_CERTS_DIRECTORY="${VOLUMES_DIRECTORY}/stroom-proxy/certs"
     mkdir -p "${DEST_PROXY_CERTS_DIRECTORY}"
-    cp ${SRC_CERTS_DIRECTORY}/certificate-authority/ca.jks ${DEST_PROXY_CERTS_DIRECTORY}
-    cp ${SRC_CERTS_DIRECTORY}/server/server.jks ${DEST_PROXY_CERTS_DIRECTORY}
+    cp "${SRC_CERTS_DIRECTORY}/certificate-authority/ca.jks" "${DEST_PROXY_CERTS_DIRECTORY}"
+    cp "${SRC_CERTS_DIRECTORY}/server/server.jks" "${DEST_PROXY_CERTS_DIRECTORY}"
 
-    local -r DEST_AUTH_UI_CERTS_DIRECTORY="${WORKING_DIRECTORY}/volumes/auth-ui/certs"
+    local -r DEST_AUTH_UI_CERTS_DIRECTORY="${VOLUMES_DIRECTORY}/auth-ui/certs"
     mkdir -p "${DEST_AUTH_UI_CERTS_DIRECTORY}"
-    cp ${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt ${DEST_AUTH_UI_CERTS_DIRECTORY}
-    cp ${SRC_CERTS_DIRECTORY}/server/server.pem.crt ${DEST_AUTH_UI_CERTS_DIRECTORY}
-    cp ${SRC_CERTS_DIRECTORY}/server/server.unencrypted.key ${DEST_AUTH_UI_CERTS_DIRECTORY}
+    cp "${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt" "${DEST_AUTH_UI_CERTS_DIRECTORY}"
+    cp "${SRC_CERTS_DIRECTORY}/server/server.pem.crt" "${DEST_AUTH_UI_CERTS_DIRECTORY}"
+    cp "${SRC_CERTS_DIRECTORY}/server/server.unencrypted.key" "${DEST_AUTH_UI_CERTS_DIRECTORY}"
 
-    local -r DEST_AUTH_UI_CONF_DIRECTORY="${WORKING_DIRECTORY}/volumes/auth-ui/conf"
+    local -r DEST_AUTH_UI_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/auth-ui/conf"
     mkdir -p "${DEST_AUTH_UI_CONF_DIRECTORY}"
-    cp ${SRC_AUTH_UI_CONF_DIRECTORY}/* ${DEST_AUTH_UI_CONF_DIRECTORY}
+    cp "${SRC_AUTH_UI_CONF_DIRECTORY}/nginx.conf.template" "${DEST_AUTH_UI_CONF_DIRECTORY}"
 
-    local -r DEST_NGINX_CERTS_DIRECTORY="${WORKING_DIRECTORY}/volumes/nginx/certs"
+    local -r DEST_NGINX_CERTS_DIRECTORY="${VOLUMES_DIRECTORY}/nginx/certs"
     mkdir -p "$DEST_NGINX_CERTS_DIRECTORY"
-    cp ${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt ${DEST_NGINX_CERTS_DIRECTORY}
-    cp ${SRC_CERTS_DIRECTORY}/server/server.pem.crt ${DEST_NGINX_CERTS_DIRECTORY}
-    cp ${SRC_CERTS_DIRECTORY}/server/server.unencrypted.key ${DEST_NGINX_CERTS_DIRECTORY}
+    cp "${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt" "${DEST_NGINX_CERTS_DIRECTORY}"
+    cp "${SRC_CERTS_DIRECTORY}/server/server.pem.crt" "${DEST_NGINX_CERTS_DIRECTORY}"
+    cp "${SRC_CERTS_DIRECTORY}/server/server.unencrypted.key" "${DEST_NGINX_CERTS_DIRECTORY}"
 
-    local -r DEST_NGINX_CONF_DIRECTORY="${WORKING_DIRECTORY}/volumes/nginx/conf"
+    local -r DEST_NGINX_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/nginx/conf"
     mkdir -p "${DEST_NGINX_CONF_DIRECTORY}"
-    cp ${SRC_NGINX_CONF_DIRECTORY}/* ${DEST_NGINX_CONF_DIRECTORY}
+    cp "${SRC_NGINX_CONF_DIRECTORY}/nginx.conf.template" "${DEST_NGINX_CONF_DIRECTORY}"
 
     # Set up the client certs needed for the send_data script
     local -r DEST_CLIENT_CERTS_DIRECTORY="${WORKING_DIRECTORY}/certs"
     mkdir -p "${DEST_CLIENT_CERTS_DIRECTORY}"
-    cp ${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt ${DEST_CLIENT_CERTS_DIRECTORY}
-    cp ${SRC_CERTS_DIRECTORY}/client/client.pem.crt ${DEST_CLIENT_CERTS_DIRECTORY}
-    cp ${SRC_CERTS_DIRECTORY}/client/client.unencrypted.key ${DEST_CLIENT_CERTS_DIRECTORY}
+    cp "${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt" "${DEST_CLIENT_CERTS_DIRECTORY}"
+    cp "${SRC_CERTS_DIRECTORY}/client/client.pem.crt" "${DEST_CLIENT_CERTS_DIRECTORY}"
+    cp "${SRC_CERTS_DIRECTORY}/client/client.unencrypted.key" "${DEST_CLIENT_CERTS_DIRECTORY}"
 
     # Get the send_to_stroom* scripts we need for the send_data script
     local -r DEST_LIB_DIR="${WORKING_DIRECTORY}/lib"
-    download_file ${DEST_LIB_DIR} ${SEND_TO_STROOM_URL_BASE} "send_to_stroom.sh"
-    download_file ${DEST_LIB_DIR} ${SEND_TO_STROOM_URL_BASE} "send_to_stroom_args.sh"
+    download_file "${DEST_LIB_DIR}" "${SEND_TO_STROOM_URL_BASE}" "send_to_stroom.sh"
+    download_file "${DEST_LIB_DIR}" "${SEND_TO_STROOM_URL_BASE}" "send_to_stroom_args.sh"
 
     # If elasticsearch is in the list of services add its volume
-    if [[ " ${services[@]} " =~ " elasticsearch " ]]; then
-        local -r DEST_ELASTIC_CONF_DIRECTORY="${WORKING_DIRECTORY}/volumes/elasticsearch/conf"
+    if [[ " ${services[@]} " =~ \\selasticsearch\\s ]]; then
+        local -r DEST_ELASTIC_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/elasticsearch/conf"
         mkdir -p "${DEST_ELASTIC_CONF_DIRECTORY}"
-        cp ${SRC_ELASTIC_CONF_DIRECTORY}/* ${DEST_ELASTIC_CONF_DIRECTORY}
+        cp "${SRC_ELASTIC_CONF_DIRECTORY}/*" "${DEST_ELASTIC_CONF_DIRECTORY}"
     fi
 
     # If stroom-log-sender is in the list of services add its volume
-    if [[ " ${services[@]} " =~ " stroomLogSender " ]]; then
+    if [[ " ${services[@]} " =~ \\sstroomLogSender\\s ]]; then
 
-        local -r DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY="${WORKING_DIRECTORY}/volumes/stroom-log-sender/certs"
+        local -r DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY="${VOLUMES_DIRECTORY}/stroom-log-sender/certs"
         mkdir -p "${DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY}"
-        cp ${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt ${DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY}
-        cp ${SRC_CERTS_DIRECTORY}/client/client.pem.crt ${DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY}
-        cp ${SRC_CERTS_DIRECTORY}/client/client.unencrypted.key ${DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY}
+        cp "${SRC_CERTS_DIRECTORY}/certificate-authority/ca.pem.crt" "${DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY}"
+        cp "${SRC_CERTS_DIRECTORY}/client/client.pem.crt" "${DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY}"
+        cp "${SRC_CERTS_DIRECTORY}/client/client.unencrypted.key" "${DEST_STROOM_LOG_SENDER_CERTS_DIRECTORY}"
 
-        local -r DEST_STROOM_LOG_SENDER_CONF_DIRECTORY="${WORKING_DIRECTORY}/volumes/stroom-log-sender/conf"
+        local -r DEST_STROOM_LOG_SENDER_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/stroom-log-sender/conf"
         mkdir -p "${DEST_STROOM_LOG_SENDER_CONF_DIRECTORY}"
-        cp ${SRC_STROOM_LOG_SENDER_CONF_DIRECTORY}/* ${DEST_STROOM_LOG_SENDER_CONF_DIRECTORY}
+        cp "${SRC_STROOM_LOG_SENDER_CONF_DIRECTORY}/*" "${DEST_STROOM_LOG_SENDER_CONF_DIRECTORY}"
     fi
 
     # If kibana is in the list of services add its volume
-    if [[ " ${services[@]} " =~ " kibana " ]]; then
-        local -r DEST_KIBANA_CONF_DIRECTORY="${WORKING_DIRECTORY}/volumes/kibana/conf"
+    if [[ " ${services[@]} " =~ \\skibana\\s ]]; then
+        local -r DEST_KIBANA_CONF_DIRECTORY="${VOLUMES_DIRECTORY}/kibana/conf"
         mkdir -p "${DEST_KIBANA_CONF_DIRECTORY}"
-        cp ${SRC_KIBANA_CONF_DIRECTORY}/* ${DEST_KIBANA_CONF_DIRECTORY}
+        cp "${SRC_KIBANA_CONF_DIRECTORY}/*" "${DEST_KIBANA_CONF_DIRECTORY}"
     fi
 
 }
--- bin/stack/create_stack_yaml.sh
+++ bin/stack/create_stack_yaml.sh
@@ -3,16 +3,18 @@
 # Use this script to load a SQL database dump into a temporary database.
 # This is useful for testing database migrations.
 
+set -e
+
 source lib/shell_utils.sh
 
 validate_requested_services() {
     # TODO: Rename the yaml to reflect the container names, i.e. from camel case to dashed.
     local -r VALID_SERVICES='stroom stroomAllDbs zookeeper stroomStats stroomQueryElasticUi stroomQueryElasticService stroomProxyLocal stroomAuthUi stroomAuthService stroomAnnotationsUi stroomAnnotationsService stroomLogSender nginx kibana kafka hbase fakeSmtp elasticsearch hdfs'
 
-    for service in "${@:2}"; do
-        if [[ $VALID_SERVICES != *$service* ]]; then
+    for service in "${@}"; do
+        if [[ ${VALID_SERVICES} != *${service}* ]]; then
             services_are_valid=false
-            err "${RED}'$service'${NC} is not a valid service!"
+            err "${RED}'${service}'${NC} is not a valid service!"
             exit 1
         fi
     done
@@ -23,11 +25,11 @@ create_stack_from_services() {
     echo version: \'2.4\'
     echo services:
     for service in "$@"; do
-        local target_yaml="$PATH_TO_CONTAINERS/$service.yml"
+        local target_yaml="${PATH_TO_CONTAINERS}/${service}.yml"
         # TODO: make this a single grep
         local service
-        service=$(grep -v '^services' "$target_yaml" | grep -v '^version:')
-        echo "$service"
+        service=$(grep -v '^services' "${target_yaml}" | grep -v '^version:')
+        echo "${service}"
     done
 }
 
@@ -43,23 +45,24 @@ append_shared_volumes() {
 main() {
     setup_echo_colours
 
-    local -r BUILD_FOLDER='build'
-    local -r STACK_NAME=$1
-    local -r WORKING_DIRECTORY="$BUILD_FOLDER/$STACK_NAME/config"
+    local -r BUILD_STACK_NAME=$1
+    local -r VERSION=$2
+    local -r BUILD_DIRECTORY="build/${BUILD_STACK_NAME}"
+    local -r WORKING_DIRECTORY="${BUILD_DIRECTORY}/${BUILD_STACK_NAME}-${VERSION}/config"
+
     mkdir -p $WORKING_DIRECTORY
-    local -r OUTPUT_FILE="$WORKING_DIRECTORY/$STACK_NAME.yml"
-    mkdir -p $WORKING_DIRECTORY
-    validate_requested_services "${@}"
+    local -r OUTPUT_FILE="${WORKING_DIRECTORY}/${BUILD_STACK_NAME}.yml"
+    validate_requested_services "${@:3}"
 
     if [ -z ${services_are_valid+x} ]; then
-        echo -e "${GREEN}Creating a stack called ${YELLOW}${STACK_NAME}${GREEN} with the following services:"
-        for service in "${@:2}"; do
+        echo -e "${GREEN}Creating a stack called ${YELLOW}${BUILD_STACK_NAME}${GREEN} with version ${YELLOW}${VERSION}${GREEN} and the following services:${NC}"
+        for service in "${@:3}"; do
             echo -e "  ${BLUE}${service}${NC}"
         done
-        create_stack_from_services "${@:2}" > "$OUTPUT_FILE"
+        create_stack_from_services "${@:3}" > "${OUTPUT_FILE}"
         append_shared_volumes
     else
-        err "Please choose from the following services and try again: ${GREEN}$VALID_SERVICES${NC}"
+        err "Please choose from the following services and try again: ${GREEN}${VALID_SERVICES}${NC}"
         exit 1
     fi
 }
