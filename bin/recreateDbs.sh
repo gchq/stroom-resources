9ce4b3d57dca529f500e4439e8037789115cbb19
at055612 <at055612@gmail.com>
2019-01-09 12:49:56 +0000

    Change recreateDbs.sh to work with stroom-all-dbs

--- bin/recreateDbs.sh
+++ bin/recreateDbs.sh
@@ -1,32 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 # 'xargs -r' doesn't work on MacOS, see 
 # https://stackoverflow.com/questions/17402345/ignore-empty-results-for-xargs-in-mac-os-x
 # Suggest you use GNU xargs from 'brew install findutils'
 
-echo "Recreating stroom-db and stroom-stats-db"
+#Shell Colour constants for use in 'echo -e'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
 
 # Doesn't touch auth-db to keep any password changes intact
 
-docker stop stroom-db
-docker stop stroom-stats-db
+echo -e "${GREEN}Stopping container ${BLUE}stroom-all-dbs${NC}"
+docker stop stroom-all-dbs
 
-docker rm stroom-db
-docker rm stroom-stats-db
+echo -e "${GREEN}Removing container ${BLUE}stroom-all-dbs${NC}"
+docker rm stroom-all-dbs
 
-# Remove the externalised database volumes
-while true; do
-    read -p "Do you wish to clean up the local dev database volumes (will prompt for root pw)? " yn
-    case $yn in
-        [Yy]* ) 
-            sudo rm -rf ~/.stroom/db_data/stroom-db
-            sudo rm -rf ~/.stroom/db_data/stroom-stats-db
-            break;;
-        [Nn]* ) 
-            exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+echo -e "${GREEN}Removing volume ${BLUE}everything_stroom-all-dbs_data${NC}"
+docker volume rm everything_stroom-all-dbs_data
 
-./bounceIt.sh create -y -i stroom-db stroom-stats-db
-./bounceIt.sh start -y -i stroom-db stroom-stats-db
+echo -e "${GREEN}Removing volume ${BLUE}everything_stroom-all-dbs_logs${NC}"
+docker volume rm everything_stroom-all-dbs_logs
+
+echo -e "${GREEN}Creating container ${BLUE}stroom-all-dbs${NC}"
+./bounceIt.sh create -y -i stroom-all-dbs
+
+echo -e "${GREEN}Starting container ${BLUE}stroom-all-dbs${NC}"
+./bounceIt.sh start -y -i stroom-all-dbs
