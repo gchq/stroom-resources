443c424f8cc409770461a660e785de07340a6956
at055612 <at055612@users.noreply.github.com>
2019-02-28 14:27:35 +0000

    Tidy up travis.before.sh script, echo existing docker-compose version

--- travis.before.sh
+++ travis.before.sh
@@ -3,13 +3,16 @@
 #exit script on any error
 set -e
 
-#Shell Colour constants for use in 'echo -e'
-#e.g.  echo -e "My message ${GREEN}with just this text in green${NC}"
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
-NC='\033[0m' # No Colour 
+# shellcheck disable=SC2034
+{
+  #Shell Colour constants for use in 'echo -e'
+  #e.g.  echo -e "My message ${GREEN}with just this text in green${NC}"
+  RED='\033[1;31m'
+  GREEN='\033[1;32m'
+  YELLOW='\033[1;33m'
+  BLUE='\033[1;34m'
+  NC='\033[0m' # No Colour 
+}
 
 DOCKER_COMPOSE_VERSION=1.23.1
 
@@ -18,13 +21,18 @@ sudo bash -c "echo '127.0.0.1 hbase' >> /etc/hosts"
 
 echo -e "TRAVIS_EVENT_TYPE:   [${GREEN}${TRAVIS_EVENT_TYPE}${NC}]"
 
+echo -e "${GREEN}Checking existing docker-compose version:${NC}"
+echo -e "${BLUE}$(docker-compose -v)${NC}"
+
 # The version of compose that is ready installed with travis does not support v2.4 yml syntax
 # so we have to update to something more recent.
 echo -e "${GREEN}Removing docker-compose${NC}"
 sudo rm /usr/local/bin/docker-compose
 
-echo -e "${GREEN}Installing docker-compose ${BLUE}${DOCKER_COMPOSER_VERSION}${NC}"
-curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+echo -e "${GREEN}Installing docker-compose ${BLUE}${DOCKER_COMPOSE_VERSION}${NC}"
+curl \
+  -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
+  > docker-compose
 chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
 
