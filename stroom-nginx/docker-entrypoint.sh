19e5481826fb7c91a4b66da888493b3e3ab5b2fb
at055612 <at055612@users.noreply.github.com>
2019-04-25 16:30:05 +0100

    Change how nginx entrypoint does tag substitution

--- stroom-nginx/docker-entrypoint.sh
+++ stroom-nginx/docker-entrypoint.sh
@@ -1,65 +1,60 @@
-#!/bin/sh
-
-BASE_DIR="/stroom-nginx"
-LOGS_DIR="${BASE_DIR}/logs"
-CONFIG_DIR="${BASE_DIR}/config"
-LOG_SENDER_HEADERS_FILE="${LOGS_DIR}/extra_headers.txt"
+#!/bin/bash
 
 set -e
 
-echo "Substituting variables to generate nginx.conf"
+create_nginx_conf() {
+  echo "Substituting tags in ${nginx_conf_template_file} to generate ${nginx_conf_file}"
 
-# We need to create nginx.conf from nginx.conf.template, 
-# substituting NGINX_ADVERTISED_HOST from its environment variable
-# As nginx.conf contains stuff like '$proxy_add_x_forwarded_for', 
-# we need to give envsubst a specific list of variables
-# to substitute, else it will break the config
-# shellcheck disable=SC2016
-envsubst '
-    ${NGINX_ADVERTISED_HOST}
-    ${STROOM_HOST}
-    ${STROOM_PORT}
-    ${STROOM_PROXY_HOST}
-    ${STROOM_PROXY_PORT}
-    ${AUTH_SERVICE_HOST}
-    ${AUTH_SERVICE_PORT}
-    ${AUTH_UI_URL}
-    ${ANNOTATIONS_UI_URL}
-    ${QUERY_ELASTIC_UI_URL}
-    ${NGINX_HTML_ROOT_PATH}
-    ${NGINX_SSL_VERIFY_CLIENT}
-    ${NGINX_SSL_CERTIFICATE}
-    ${NGINX_SSL_CERTIFICATE_KEY}
-    ${NGINX_SSL_CLIENT_CERTIFICATE}
-    ${NGINX_CLIENT_BODY_BUFFER_SIZE}
-    ' \
-    < "${CONFIG_DIR}/nginx.conf.template" \
-    > /etc/nginx/nginx.conf
+  cat "${nginx_conf_template_file}" > "${nginx_conf_file}"
 
-echo "Ensuring directories"
-# Ensure we have the sub-directories in our /nginx/logs/ volume
-mkdir -p "${LOGS_DIR}/access"
-mkdir -p "${LOGS_DIR}/app"
+  # Find all the distinct <<<SOME_TAG>>> type tags in the template file
+  unique_tags_in_file="$( \
+    grep -oP "<<<[^<]+>>>" "${nginx_conf_template_file}" \
+    | sort \
+    | uniq \
+  )"
 
-# shellcheck source=add_container_identity_headers.sh
-. "${BASE_DIR}/add_container_identity_headers.sh" "${LOG_SENDER_HEADERS_FILE}"
+  is_tag_replacement_missing=false
 
-crontab_file="${CONFIG_DIR}/crontab.txt"
-logrotate_template_file="${CONFIG_DIR}/logrotate.conf.template"
-logrotate_conf_file="${BASE_DIR}/logrotate/logrotate.conf"
+  if [ -n "${unique_tags_in_file}" ]; then
+    while read -r tag; do
+      local tag_name
+      tag_name="$(echo "${tag}" | sed -E 's/[<>]{3}//g')"
 
-if [ -f "${logrotate_template_file}" ]; then
+      local replacement_value="${!tag}"
+
+      if [ -n "${replacement_value}" ]; then
+        echo "  Replacing tag ${tag_name} with value [${replacement_value}]"
+
+        sed -i'' "s|${tag}|${replacement_value}|g" "${nginx_conf_file}"
+      else
+        echo "ERROR: No substitution value for tag ${tag_name}"
+        is_tag_replacement_missing=true
+      fi
+    done <<< "${unique_tags_in_file}"
+
+    if [ "${is_tag_replacement_missing}" = false ]; then
+      echo "One or more substitution tags were missing so exiting"
+      exit 1
+    fi
+  fi
+}
+
+setup_logrotate() {
+  if [ -f "${logrotate_template_file}" ]; then
 
     echo "Creating ${logrotate_conf_file} from ${logrotate_template_file}"
     cp "${logrotate_template_file}" "${logrotate_conf_file}"
     # logrotate is fussy about the ownership/permissions of the conf file
     chmod -R 400 "${logrotate_conf_file}"
 
-else
+  else
     echo "WARN: logrotate template file ${logrotate_template_file} not found, nginx logs won't be rotated"
-fi
+  fi
+}
 
-if [ -f "${crontab_file}" ]; then
+setup_crontab() {
+  if [ -f "${crontab_file}" ]; then
     echo "(Re-)setting crontab to:"
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     cat "${crontab_file}"
@@ -70,17 +65,46 @@ if [ -f "${crontab_file}" ]; then
     # we can run as non-root
     /usr/bin/crontab "${crontab_file}"
 
-
-
     # start crond as root
     echo "Starting crond in the background"
 
     /usr/sbin/crond -l 8 && \
-        echo "Starting CMD: [" "$@" "]" && \
-        exec "$@"
-else
+      echo "Starting CMD: [" "$@" "]" && \
+      exec "$@"
+  else
     echo "WARN: crontab file ${crontab_file} not found, can't start cron, nginx logs won't be rotated"
     # Now run the CMD
     echo "Starting CMD: [" "$@" "]"
     exec "$@"
-fi
+  fi
+}
+
+main() {
+  base_dir="/stroom-nginx"
+  logs_dir="${base_dir}/logs"
+  config_dir="${base_dir}/config"
+  log_sender_headers_file="${logs_dir}/extra_headers.txt"
+  crontab_file="${config_dir}/crontab.txt"
+  logrotate_template_file="${config_dir}/logrotate.conf.template"
+  logrotate_conf_file="${base_dir}/logrotate/logrotate.conf"
+
+  nginx_conf_template_file="${config_dir}/nginx.conf.template"
+  nginx_conf_file="/etc/nginx/nginx.conf"
+
+  create_nginx_conf
+
+  echo "Ensuring directories"
+  # Ensure we have the sub-directories in our /nginx/logs/ volume
+  mkdir -p "${logs_dir}/access"
+  mkdir -p "${logs_dir}/app"
+
+  # shellcheck disable=SC1090
+  . "${base_dir}/add_container_identity_headers.sh" "${log_sender_headers_file}"
+
+  setup_logrotate
+  setup_crontab "$@"
+}
+
+main "$@"
+
+# vim: set shiftwidth=2 tabstop=2:
