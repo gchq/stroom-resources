c0492195ebf08bd3c656ad7a51a7ab450d515c38
at055612 <at055612@users.noreply.github.com>
2019-04-26 11:59:58 +0100

    Fix nginx conf tag substitution, add colour to entrypoint script

--- stroom-nginx/docker-entrypoint.sh
+++ stroom-nginx/docker-entrypoint.sh
@@ -2,63 +2,76 @@
 
 set -e
 
+setup_colours() {
+  # Shell Colour constants for use in 'echo -e'
+  # e.g.  echo -e "My message ${GREEN}with just this text in green${NC}"
+  # shellcheck disable=SC2034
+  {
+    RED='\033[1;31m'
+    GREEN='\033[1;32m'
+    YELLOW='\033[1;33m'
+    BLUE='\033[1;34m'
+    NC='\033[0m' # No Colour 
+  }
+}
+
 create_nginx_conf() {
-  echo "Substituting tags in ${nginx_conf_template_file} to generate ${nginx_conf_file}"
+  echo -e "Substituting tags in ${BLUE}${nginx_conf_template_file}${NC}" \
+    "to generate ${BLUE}${nginx_conf_file}${NC}"
 
   cat "${nginx_conf_template_file}" > "${nginx_conf_file}"
 
   # Find all the distinct <<<SOME_TAG>>> type tags in the template file
   unique_tags_in_file="$( \
-    grep -oP "<<<[^<]+>>>" "${nginx_conf_template_file}" \
+    grep -oE "<<<[^<]+>>>" "${nginx_conf_template_file}" \
     | sort \
     | uniq \
   )"
 
-  is_tag_replacement_missing=false
-
   if [ -n "${unique_tags_in_file}" ]; then
     while read -r tag; do
       local tag_name
-      tag_name="$(echo "${tag}" | sed -E 's/[<>]{3}//g')"
+      tag_name="$(echo -e "${tag}" | sed -E 's/[<>]{3}//g')"
+      #echo -e "tag_name: ${tag_name}"
 
-      local replacement_value="${!tag}"
+      local replacement_value="${!tag_name}"
+      #echo -e "replacement_value: ${replacement_value}"
 
-      if [ -n "${replacement_value}" ]; then
-        echo "  Replacing tag ${tag_name} with value [${replacement_value}]"
+      echo -e "  Replacing tag ${YELLOW}${tag_name}${NC} with value" \
+        "[${GREEN}${replacement_value}${NC}]"
 
-        sed -i'' "s|${tag}|${replacement_value}|g" "${nginx_conf_file}"
-      else
-        echo "ERROR: No substitution value for tag ${tag_name}"
-        is_tag_replacement_missing=true
+      sed -i'' "s|${tag}|${replacement_value}|g" "${nginx_conf_file}"
+
+      if [ ! -n "${replacement_value}" ]; then
+        echo -e "  ${RED}WARN${NC}: No substitution value for" \
+          "tag ${YELLOW}${tag_name}${NC}, nginx may not start!"
       fi
     done <<< "${unique_tags_in_file}"
-
-    if [ "${is_tag_replacement_missing}" = false ]; then
-      echo "One or more substitution tags were missing so exiting"
-      exit 1
-    fi
   fi
 }
 
 setup_logrotate() {
   if [ -f "${logrotate_template_file}" ]; then
 
-    echo "Creating ${logrotate_conf_file} from ${logrotate_template_file}"
+    echo -e "Creating ${BLUE}${logrotate_conf_file}${NC} from" \
+      "${BLUE}${logrotate_template_file}${NC}"
     cp "${logrotate_template_file}" "${logrotate_conf_file}"
     # logrotate is fussy about the ownership/permissions of the conf file
     chmod -R 400 "${logrotate_conf_file}"
 
   else
-    echo "WARN: logrotate template file ${logrotate_template_file} not found, nginx logs won't be rotated"
+    echo -e "${RED}WARN${NC}: logrotate template file" \
+      "${BLUE}${logrotate_template_file}${NC}" \
+      "not found, nginx logs won't be rotated"
   fi
 }
 
-setup_crontab() {
+setup_crontab_and_run_cmd() {
   if [ -f "${crontab_file}" ]; then
-    echo "(Re-)setting crontab to:"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo -e "(Re-)setting crontab to:"
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     cat "${crontab_file}"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     # If we assign the crontab to the 'sender' user (crontab -u ...) it won't work, 
     # as sender dosn't have perms on /dev/stdout
     # Instead, consider using supercronic - https://github.com/aptible/supercronic/ so that
@@ -66,15 +79,16 @@ setup_crontab() {
     /usr/bin/crontab "${crontab_file}"
 
     # start crond as root
-    echo "Starting crond in the background"
+    echo -e "Starting crond in the background"
 
     /usr/sbin/crond -l 8 && \
-      echo "Starting CMD: [" "$@" "]" && \
+      echo -e "Starting CMD: [" "$@" "]" && \
       exec "$@"
   else
-    echo "WARN: crontab file ${crontab_file} not found, can't start cron, nginx logs won't be rotated"
+    echo -e "${RED}WARN${NC}: crontab file ${BLUE}${crontab_file}${NC} not" \
+      "found, can't start cron, nginx logs won't be rotated"
     # Now run the CMD
-    echo "Starting CMD: [" "$@" "]"
+    echo -e "Starting CMD: [" "$@" "]"
     exec "$@"
   fi
 }
@@ -91,9 +105,10 @@ main() {
   nginx_conf_template_file="${config_dir}/nginx.conf.template"
   nginx_conf_file="/etc/nginx/nginx.conf"
 
+  setup_colours
   create_nginx_conf
 
-  echo "Ensuring directories"
+  echo -e "Ensuring directories"
   # Ensure we have the sub-directories in our /nginx/logs/ volume
   mkdir -p "${logs_dir}/access"
   mkdir -p "${logs_dir}/app"
@@ -102,7 +117,7 @@ main() {
   . "${base_dir}/add_container_identity_headers.sh" "${log_sender_headers_file}"
 
   setup_logrotate
-  setup_crontab "$@"
+  setup_crontab_and_run_cmd "$@"
 }
 
 main "$@"
