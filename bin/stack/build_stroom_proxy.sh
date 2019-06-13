f05a83b49000be1c566e33f4d34c11417a4d1b65
at055612 <at055612@users.noreply.github.com>
2019-06-13 13:53:27 +0100

    Add a proxy-remote only stack definition

---
+++ bin/stack/build_stroom_proxy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Builds a core Stroom stack using the default configuration from the yaml.
+
+set -e
+
+main() {
+    local -r VERSION=$1
+    local -r BUILD_STACK_NAME="stroom_proxy"
+
+    local SERVICES=()
+
+    # Define all the services that make up the stack
+    # Array created like this to allow lines to commneted out
+    SERVICES+=("stroom-proxy-remote")
+
+    ./build.sh "${BUILD_STACK_NAME}" "${VERSION:-SNAPSHOT}" "${SERVICES[@]}"
+}
+
+main "$@"
