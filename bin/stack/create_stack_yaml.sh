03f613d048711adadad8096ffd294f247c6e0bec
at055612 <at055612@users.noreply.github.com>
2019-12-09 15:29:14 +0000

    Split volumes into separate yaml files (WIP, broken)

--- bin/stack/create_stack_yaml.sh
+++ bin/stack/create_stack_yaml.sh
@@ -7,7 +7,7 @@ set -e
 # shellcheck disable=SC1091
 source lib/shell_utils.sh
 
-create_stack_from_services() {
+old_create_stack_from_services() {
   local -r PATH_TO_CONTAINERS="../compose/containers"
   echo version: \'2.4\'
   echo services:
@@ -20,42 +20,79 @@ create_stack_from_services() {
   done
 }
 
-append_shared_volumes() {
-  echo -e "${GREEN}Adding volumes to docker compose YAML file${NC}"
-  local -r MASTER_YAML="../compose/everything.yml"
+create_stack_from_services() {
 
-  # Read the volume whitelist into an array
-  local volume_whitelist=()
-  if [ -f "${VOLUMES_WHITELIST_FILE}" ]; then
-    while read line; do
-      # Add a colon to the end as that is how we see them in the master yml file
-      volume_whitelist+=("${line}")
-    done < "${VOLUMES_WHITELIST_FILE}"
-  else
-    echo -e "${YELLOW}WARN${NC}: Volume whitelist file ${BLUE}${VOLUMES_WHITELIST_FILE}${NC} not found."
-    echo -e "No docker volumes will be added to the stack"
-  fi
+  local compose_args=()
+  local target_file
 
-  # grab all content from master yaml file after and including 'volumes:' entry
-  # and ignoring comments and blank lines
-  local all_volumes
-  all_volumes="$(sed -n -e '/^\w*volumes:/,$p' "${MASTER_YAML}" |
-    grep -vP '^\w*#' | 
-    grep -v '^\w*$' )"
+  # Add in the special root file first. This MUST come first
+  # as all other paths are relative to this.
+  compose_args+=( "-f" "${ROOT_YAML_FILE}" )
 
-  echo "${all_volumes}" | while read vol; do
-    if [ "${vol}" = "volumes:" ]; then
-      echo "${vol}" >> "${OUTPUT_FILE}"
-    else
-      local trimmed_vol="${vol%%:}"
-      if element_in "${trimmed_vol}" "${volume_whitelist[@]}"; then
-        echo -e "  ${BLUE}${trimmed_vol}${NC}"
+  echo "Named vols"
+  # Add any named volume files first (if the service has one)
+  for service in "${SERVICES[@]}"; do
+    target_file="${NAMED_VOLUMES_DIR}/${service}${NAMED_VOLUMES_SUFFIX}.yml"
 
-        # indent is important here as it is yaml
-        echo "  ${vol}" >> "${OUTPUT_FILE}"
-      fi
+    if [ -f "${target_file}" ]; then
+      # path needs to be relative to home of root yml file
+      #compose_args+=( \
+        #"-f" \
+        #"$(realpath --relative-to="${CONTAINERS_DIR}" "${target_file}")" \
+      #)
+      #echo "  $(realpath --relative-to="${CONTAINERS_DIR}" "${target_file}")"
+
+      compose_args+=( "-f" "${target_file}" )
+      echo "  ${target_file}"
     fi
   done
+
+  echo "Service files"
+  # Add the actual service files
+  for service in "${SERVICES[@]}"; do
+    target_file="${CONTAINERS_DIR}/${service}.yml"
+    #compose_args+=( \
+      #"-f" \
+      #"$(realpath --relative-to="${CONTAINERS_DIR}" "${target_file}")" \
+      #)
+    #echo "  $(realpath --relative-to="${CONTAINERS_DIR}" "${target_file}")"
+    compose_args+=( "-f" "${target_file}" )
+    echo "  ${target_file}"
+  done
+
+  echo "Override files"
+  # Add any volume mount files if they are applicable for this stack
+  for vol_mount_file in ${NAMED_VOLUME_MOUNTS_DIR}/*.yml; do
+    # filename format is serviceX_serviceY.yml, where serviceX is the service
+    # that is having the volume mount and serviceY is the service it is sharing
+    # it with.
+    local filename
+    filename="$(basename "${vol_mount_file}" )"
+    filename="${filename%%\.yml}"
+    local target_service="${filename%%_*}"
+    local source_service="${filename##*_}"
+
+    echo "[${target_service}] [${source_service}]"
+
+    if element_in "${target_service}" "${SERVICES[@]}" \
+      && element_in "${source_service}" "${SERVICES[@]}"; then
+      # both target and source are in the stack servcies so include this
+      # override file
+
+      #compose_args+=( \
+        #"-f" \
+        #"$(realpath --relative-to="${CONTAINERS_DIR}" "${vol_mount_file}")" \
+      #)
+      #echo "  $(realpath --relative-to="${CONTAINERS_DIR}" "${vol_mount_file}")"
+      compose_args+=( "-f" "${vol_mount_file}" )
+      echo "  ${vol_mount_file}"
+    fi
+  done
+
+  echo "compose args: ${compose_args[*]}"
+  pwd
+
+  docker-compose "${compose_args[@]}" config > "${OUTPUT_FILE}"
 }
 
 main() {
@@ -65,15 +102,19 @@ main() {
   local -r VERSION=$2
   local -r SERVICES=("${@:3}")
   local -r BUILD_DIRECTORY="build/${BUILD_STACK_NAME}"
-  local -r STACK_DEFINITIONS_DIR="stack_definitions/${BUILD_STACK_NAME}"
-  local -r VOLUMES_WHITELIST_FILE="${STACK_DEFINITIONS_DIR}/volumes_whitelist.txt"
   local -r WORKING_DIRECTORY="${BUILD_DIRECTORY}/${BUILD_STACK_NAME}-${VERSION}/config"
 
   mkdir -p "$WORKING_DIRECTORY"
   local -r OUTPUT_FILE="${WORKING_DIRECTORY}/${BUILD_STACK_NAME}.yml"
+  local -r CONTAINERS_DIR="../compose/containers"
+  local -r ROOT_YAML_FILE="${CONTAINERS_DIR}/root.yml"
+  local -r NAMED_VOLUMES_DIR="${CONTAINERS_DIR}/named_volumes"
+  local -r NAMED_VOLUME_MOUNTS_DIR="${CONTAINERS_DIR}/named_volume_mounts"
 
-  create_stack_from_services "${SERVICES[@]}" > "${OUTPUT_FILE}"
-  append_shared_volumes
+  local -r NAMED_VOLUMES_SUFFIX="_volumes"
+
+  #create_stack_from_services "${SERVICES[@]}" > "${OUTPUT_FILE}"
+  create_stack_from_services "${SERVICES[@]}"
 }
 
 main "$@"
