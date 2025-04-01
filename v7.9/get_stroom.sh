6c4d45b915610e610149def870e7cbbbbae02eb7
stroomdev66 <stroomdev66@bots.github.com>
2025-04-01 20:14:10 +0000

    Updated GitHub Pages

--- v7.9/get_stroom.sh
+++ v7.9/get_stroom.sh
@@ -22,7 +22,7 @@
 
 # This script is templated and will be substituted during the build process
 # To test the templated version you can do something like this:
-# bash <(cat ~/git_work/stroom-resources/bin/stack/lib/get_stroom.sh | sed 's/stroom_core_test/stroom_core_test/; s/stroom-stacks-v7.9-beta.4/stroom-stacks-v6.0-beta.28-9/; s/v7.9-beta.4/v6.0-beta.28-9/; s/2a5c02f0600c236070b0b4cafa81a524d2c75c2b3825cdce06c32e110461c62a  stroom_core_test-v7.9-beta.4.tar.gz/fc593474e2ee6b9a7f507303fc38522c6a4d1abf62e2ddc7af0f20d15b6baeb0  stroom_core_test-v6.0-beta.28-9.tar.gz/' )
+# bash <(cat ~/git_work/stroom-resources/bin/stack/lib/get_stroom.sh | sed 's/stroom_core_test/stroom_core_test/; s/stroom-stacks-v7.9-beta.4-annotations3/stroom-stacks-v6.0-beta.28-9/; s/v7.9-beta.4-annotations3/v6.0-beta.28-9/; s/82d2d680632f14a14b01bd113d5d4ff261f6b85141a598747cb9e9142c24c975  stroom_core_test-v7.9-beta.4-annotations3.tar.gz/fc593474e2ee6b9a7f507303fc38522c6a4d1abf62e2ddc7af0f20d15b6baeb0  stroom_core_test-v6.0-beta.28-9.tar.gz/' )
 # replacing the sed replacements
 
 # Exit the script on any error
@@ -40,9 +40,9 @@ NC='\033[0m' # No Colour
 main() {
   # stack_version will be hard coded by TravisCI at build time
   local -r stack_name="stroom_core_test"
-  local -r stack_tag="stroom-stacks-v7.9-beta.4"
-  local -r stack_version="v7.9-beta.4"
-  local -r hash_file_contents="2a5c02f0600c236070b0b4cafa81a524d2c75c2b3825cdce06c32e110461c62a  stroom_core_test-v7.9-beta.4.tar.gz"
+  local -r stack_tag="stroom-stacks-v7.9-beta.4-annotations3"
+  local -r stack_version="v7.9-beta.4-annotations3"
+  local -r hash_file_contents="82d2d680632f14a14b01bd113d5d4ff261f6b85141a598747cb9e9142c24c975  stroom_core_test-v7.9-beta.4-annotations3.tar.gz"
 
   local -r install_dir="./${stack_name}/${stack_tag}"
   local -r archive_filename="${stack_name}-${stack_version}.tar.gz"
