ac679eeeea394924017c4e18f3ee94ce70b10720
at055612 <22818309+at055612@users.noreply.github.com>
2026-06-12 14:42:47 +0100

    Add su-exec drop down, change chmod to octal

--- build_local_log_sender_image.sh
+++ build_local_log_sender_image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DOCKER_IMAGE_TAG="local-SNAPSHOT"
+GIT_TAG="${DOCKER_IMAGE_TAG}"
+
 # Check for uncommitted work
 if [ "$(git status --porcelain 2>/dev/null | wc -l)" -eq 0 ]; then
     GIT_COMMIT="$(git rev-parse HEAD)"
@@ -10,4 +13,5 @@ fi
 docker build \
     --tag gchq/stroom-log-sender:local-SNAPSHOT \
     --build-arg GIT_COMMIT="${GIT_COMMIT}" \
+    --build-arg GIT_TAG="${GIT_TAG}" \
     ./stroom-log-sender
--- stroom-log-sender/docker-entrypoint.sh
+++ stroom-log-sender/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-# This is the file in the bind mounted volume with the env vars in it
-crontab_file=/stroom-log-sender/config/crontab.txt
 
-if [ ! -f "${crontab_file}" ]; then
-  echo "Error: source crontab file ${crontab_file} not found."
+# These should be set in the dockerfile, but just in case
+USER_ID="${USER_ID:-1000}"
+GROUP_ID="${GROUP_ID:-0}"
+# This is the file in the bind mounted volume with the env vars in it
+CRONTAB_FILE=/stroom-log-sender/config/crontab.txt
+
+
+if [ ! -f "${CRONTAB_FILE}" ]; then
+  echo "Error: source crontab file ${CRONTAB_FILE} not found."
   echo "Quitting"
   exit 1
 fi
@@ -17,7 +22,15 @@ env \
   | sort
 echo "----------------------------------------------------------"
 
-echo "Running supercronic"
-exec "$@"
 
-#echo "End of entrypoint"
+# In case anyone tries to run the container as root drop down
+if [ "$(id -u)" = '0' ]; then
+    #su-exec is the alpine equivalent of gosu
+    #runs all args as user USER_ID:GROUP_ID, rather than as root
+    echo "Running supercronic via su-exec as $USER_ID:$GROUP_ID"
+    exec su-exec "$USER_ID:$GROUP_ID" "$@"
+else
+  # Already non-root so just crack on and run the cmd as is
+  echo "Running supercronic"
+  exec "$@"
+fi
